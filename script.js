2b771d643fa09d6d4322d34270e302b7e66693ce
Jentldv <Jentldv@users.noreply.github.com>
2019-06-01 17:06:56 +0200

    Foutje JS rechtgezet

--- script.js
+++ script.js
@@ -13,41 +13,6 @@ function switchToBlue() {
 redLink.addEventListener("click",switchToRed);
 blueLink.addEventListener("click",switchToBlue);
 
-/* var tabEen = document.querySelector("#een");
-var tabTwee = document.querySelector("#twee");
-var tabDrie = document.querySelector("#drie");
-var tabVier = document.querySelector("#vier");
-
-tabEen.onclick = function() {
-      this.className = "";
-      tabTwee.className = "";
-      tabDrie.className = "";
-}
-
-tabTwee.onclick = function() {
-       if (this.className === "twee-weg") {
-             this.className = "";
-             tabDrie.className = "";
-       } else {
-            tabEen.classList.add("een-weg");
-       }
-}
-
-tabDrie.onclick = function() {
-      if (this.className === "drie-weg") {
-            this.className = "";
-      } else {
-            tabEen.classList.add("een-weg");
-            tabTwee.classList.add("twee-weg");
-      }
-}
-
-tabVier.onclick = function() {
-      tabEen.classList.add("een-weg");
-      tabTwee.classList.add("twee-weg");
-      tabDrie.classList.add("drie-weg");
-} */
-
 let loaderWrapper = document.getElementById('loader-wrapper');
 let loader = document.getElementById('loader');
 let i = 0;
